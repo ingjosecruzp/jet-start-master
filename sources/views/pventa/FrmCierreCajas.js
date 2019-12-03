e46e330032891cac9c14895e5b650864f2b4b7f6
KrOzZ <51245575+KrOzZ@users.noreply.github.com>
2019-12-03 12:22:08 -0700

    agrega cierre caja completo

--- sources/views/pventa/FrmCierreCajas.js
+++ sources/views/pventa/FrmCierreCajas.js
@@ -1,6 +1,13 @@
 import { JetView } from "webix-jet";
 import { FrmBase } from "views/FrmBase";
 import { CierreCajas } from "models/pventa/CierreCajas";
+import { getSiNo } from "models/generales";
+
+import { cajas } from "models/pventa/cajas";
+import { cajeros } from "models/pventa/cajeros";
+import { formacobro } from "models/pventa/formacobro";
+
+
 //import { almacenes } from "models/catalogos/almacenes";
 //import { getCajasRegVta, getCajasCobroDefault } from "models/generales";
 
@@ -12,16 +19,64 @@ export class FrmCierreCajas extends FrmBase {
             width: 400,
             elements: [
                 { view: "text", name: "_id", hidden: true },
-                { view: "text", name: "TipoMovto", label: "Tipo Movimiento", labelWidth: 140 },
-                { view: "datepicker", id: "Fecha" + id, label: "Fecha", labelWidth: 140, name: "Fecha", stringResult: true, format: "%d  %M %Y", value: new Date() },
+                { view: "datepicker", id: "Fecha" + id,disabled:true, label: "Fecha", labelWidth: 140, name: "Fecha", stringResult: true, format: "%d  %M  %Y", value: new Date() },
+                //{ view: "text", name: "TipoMovto", label: "Tipo Movimiento", labelWidth: 140 },
+                {
+                    view: "combo",
+                    name: "Cajas._id",
+                    labelWidth: 140,
+                    id: "cmbCajas" + id,
+                    label: "Cajas",
+                    options: {
+                        body: {
+                            template: "#Nombre#",
+                            dataFeed: function(text) {
+                                let caja = new cajas();
+                                this.load(caja.searchCombo(text));
+                            }
+                        }
+                    }
+                },
+                {
+                    view: "combo",
+                    name: "Cajeros._id",
+                    labelWidth: 140,
+                    id: "cmbCajeros" + id,
+                    label: "Cajeros",
+                    options: {
+                        body: {
+                            template: "#Nombre#",
+                            dataFeed: function(text) {
+                                let cajero = new cajeros();
+                                this.load(cajero.searchCombo(text));
+                            }
+                        }
+                    }
+                },
+                {
+                    view: "combo",
+                    name: "FormaCobro._id",
+                    labelWidth: 140,
+                    id: "cmbformacobro" + id,
+                    label: "Forma de Cobro",
+                    options: {
+                        body: {
+                            template: "#Nombre#",
+                            dataFeed: function(text) {
+                                let formcobro = new formacobro();
+                                this.load(formcobro.searchCombo(text));
+                            }
+                        }
+                    }
+                },
                 { view: "text", name: "FormaEmitida", label: "Forma Emitida", labelWidth: 140 },
                 { view: "text", name: "Importe", label: "Importe", labelWidth: 140 }
 
             ],
             rules: {
                 //$all: webix.rules.isNotEmpty
-                //"TextoImporte": webix.rules.isNotEmpty,
-                "TipoMovto": webix.rules.isNotEmpty,
+                //"Importe": webix.rules.isNotEmpty,
+                //"TipoMovto": webix.rules.isNotEmpty
                 //"Simbolo": webix.rules.isNotEmpty,
                 //"ClaveFiscal": webix.rules.isNotEmpty,
                 //"ClaveInterna": webix.rules.isNotEmpty
@@ -35,5 +90,12 @@ export class FrmCierreCajas extends FrmBase {
     }
     init(view) {
         webix.extend($$(this.Ventana), webix.ProgressBar);
+    }
+    cargarCombos(data) {
+        console.log(data);
+        //$$("Fecha" + this.id).setValue(this.convertToDate(data.Fecha));
+        this.cargarCombo(this.$$("cmbCajas" + this.id), data.Cajas);
+        this.cargarCombos(this.$$("cmbformacobro" + this.id), data.formacobro);
+        this.cargarCombao(this.$$("cmbCajeros" + this.id), data.Cajeros);
     }
 }
